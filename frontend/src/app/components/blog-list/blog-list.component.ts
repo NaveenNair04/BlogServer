f83f12ec0b2e32676f8f2fb62b3a025813fbce3e
NaveenNair04 <naveen.nair2004@gmaail.com>
2025-04-07 13:03:56 +0530

    Added search bar

--- frontend/src/app/components/blog-list/blog-list.component.ts
+++ frontend/src/app/components/blog-list/blog-list.component.ts
@@ -3,16 +3,19 @@ import { BlogService } from '../../services/blog.service';
 import { Blog } from '../../models/blog.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css'],
 })
 export class BlogListComponent implements OnInit {
   blogs: Blog[] = [];
+  filteredBlogs: Blog[] = [];
+  searchTerm: string = '';
 
   constructor(private blogService: BlogService) {}
 
@@ -20,8 +23,16 @@ export class BlogListComponent implements OnInit {
     this.blogService.getBlogs().subscribe((data) => {
       this.blogs = data.map((blog) => ({
         ...blog,
-        createdAt: new Date(blog.createdAt), // Convert string to Date
+        createdAt: new Date(blog.createdAt),
       }));
+      this.filteredBlogs = this.blogs;
     });
+  }
+
+  onSearch() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredBlogs = this.blogs.filter((blog) =>
+      blog.title.toLowerCase().includes(term),
+    );
   }
 }
